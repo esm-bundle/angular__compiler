f87307871cf7439e23d31eb7165b583448ac8a44
arturovt <arthurandrosovich@gmail.com>
2024-04-13 19:02:44 +0300

    Bump Angular to 17.3.0

--- browser-test/test.js
+++ browser-test/test.js
@@ -1,15 +1,13 @@
 describe("@esm-bundle/angular__compiler", () => {
-  ["es2015", "es2020"].forEach((ecma) => {
+  ["es2022"].forEach((ecma) => {
     it(`can load the System.register ${ecma} bundle`, async () => {
-      const m = await System.import(
-        `/base/system/${ecma}/ivy/angular-compiler.js`
-      );
+      const m = await System.import(`/base/system/${ecma}/angular-compiler.js`);
       expect(m.CompilerConfig).toBeDefined();
     });
 
     it(`can load the System.register ${ecma} prod bundle`, async () => {
       const m = await System.import(
-        `/base/system/${ecma}/ivy/angular-compiler.min.js`
+        `/base/system/${ecma}/angular-compiler.min.js`,
       );
       expect(m.CompilerConfig).toBeDefined();
     });
